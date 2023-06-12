0b7aa214cce8ba2319b5658e9ebb2afd4d20b935
issa99-lab <issanana99@gmail.com>
2023-06-12 18:51:05 +0300

    Custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+    },
+    fontFamily: {
+      sans: ['Commissioner', 'sans-serif']
+    },
+    colors: {
+      'moderate-cyan': 'hsl(176, 50%, 47%)',
+      'dark-cyan': 'hsl(176, 72%, 28%)',
+      'black': 'hsl(0, 0%, 0%)',
+      'dark-gray': 'hsl(0, 0%, 48%)',
+    },
     extend: {},
   },
   plugins: [],
