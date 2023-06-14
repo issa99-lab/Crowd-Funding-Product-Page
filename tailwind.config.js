d71a5c2e8ea124d1567f3bce3724afcdfda145d8
issa99-lab <issanana99@gmail.com>
2023-06-15 01:37:51 +0300

    /

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,8 +24,8 @@ module.exports = {
 
     extend: {
       backgroundImage: {
-        'hero-desktop': "url('./images/image-hero-desktop.jpg')",
-        'hero-mobile': "url('./images/image-hero-mobile.jpg') ",
+        'hero-desktop': "url('/Crowd-Funding-Product-Page/images/image-hero-desktop.jpg')",
+        'hero-mobile': "url('/Crowd-Funding-Product-Page/images/image-hero-mobile.jpg') ",
       }
     },
   },
