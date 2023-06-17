8875dc55db603dcb4793f8cce4fcd3781c6a67d8
issa99-lab <issanana99@gmail.com>
2023-06-17 23:40:11 +0300

    Started modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     screens: {
       sm: '640px',
       md: '768px',
-      lg: '1024px',
+      lg: '1200px',
     },
     fontFamily: {
       sans: ['Commissioner', 'sans-serif']
