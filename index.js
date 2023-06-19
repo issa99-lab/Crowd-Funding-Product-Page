621d1c893440b318474cddc1290bc4a5c6ccd01e
issa99-lab <issanana99@gmail.com>
2023-06-19 23:33:21 +0300

    JS A bit

---
+++ index.js
@@ -0,0 +1,16 @@
+// select elements on click'
+const closeButton = document.getElementById('closeBtn');
+const backProjectBtn = document.getElementById('backProject');
+const openModal = document.getElementById('openModal');
+const bodyChange = document.querySelector('body').style = "opacity-15"
+
+//close
+closeButton.addEventListener('click', () => {
+  openModal.classList.add('hidden');
+});
+
+// open
+backProjectBtn.addEventListener('click', function () {
+  openModal.classList.remove('hidden');
+  bodyChange;
+})
